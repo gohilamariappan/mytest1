a06ef68ff131b86950a42c258ede66fa7b0edfc8
Prashant Kumar <144701828+prashantesmagico@users.noreply.github.com>
2023-10-31 10:42:49 +0530

    Revert and added back the file upload module

---
+++ src/file-upload/file-upload.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreateFileUploadDto } from './dto/create-file-upload.dto';
+import { UpdateFileUploadDto } from './dto/update-file-upload.dto';
+
+@Injectable()
+export class FileUploadService {
+  
+}
