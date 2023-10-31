725dc6db2ae3f9e6c521f8dd222286735f03c6ae
VamshiBatta07 <batta.v@esmagico.in>
2023-10-31 11:20:19 +0530

    Optimizing SurveyScore module.

--- src/survey-score/survey-score.module.ts
+++ src/survey-score/survey-score.module.ts
@@ -1,29 +1,17 @@
-import { Module, forwardRef } from "@nestjs/common";
-import { QuestionBankService } from "src/question-bank/question-bank.service";
+import { Module } from "@nestjs/common";
 import { ResponseTrackerService } from "src/response-tracker/response-tracker.service";
 import { SurveyFormService } from "src/survey-form/survey-form.service";
-import { UserMetadataModule } from "src/user-metadata/user-metadata.module";
+import { QuestionBankModule } from "../question-bank/question-bank.module";
 import { SurveyScoreController } from "./survey-score.controller";
 import { SurveyScoreService } from "./survey-score.service";
-import { MockUserService } from "../mockModules/mock-user/mock-user.service";
-import { MockDesignationService } from "../mockModules/mock-designation/mock-designation.service";
-import { MockRoleService } from "../mockModules/mock-role/mock-role.service";
-import { MockCompetencyService } from "../mockModules/mock-competency/mock-competency.service";
-import { MockCompetencyLevelService } from "../mockModules/mock-competency-level/mock-competency-level.service";
 
 @Module({
-  imports: [forwardRef(() => UserMetadataModule)],
+  imports: [QuestionBankModule],
   controllers: [SurveyScoreController],
   providers: [
     SurveyScoreService,
     ResponseTrackerService,
-    QuestionBankService,
-    SurveyFormService,
-    MockUserService,
-    MockDesignationService,
-    MockRoleService,
-    MockCompetencyService,
-    MockCompetencyLevelService
+    SurveyFormService
   ],
 })
 export class SurveyScoreModule {}
