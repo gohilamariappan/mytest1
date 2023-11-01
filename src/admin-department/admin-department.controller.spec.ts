ceea2612e1f1d33a49d6ddb60e403a6d2b235315
Prashant Kumar <144701828+prashantesmagico@users.noreply.github.com>
2023-11-01 17:10:35 +0530

    Added testcases for admin

---
+++ src/admin-department/admin-department.controller.spec.ts
@@ -0,0 +1,58 @@
+import * as pactum from "pactum";
+import { PrismaService } from "../prisma/prisma.service";
+import { Test, TestingModule } from "@nestjs/testing";
+import { ConfigService } from "@nestjs/config";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { AdminDepartmentModule } from "./admin-department.module";
+import { ResponseAdminDepartmentDto } from "./dto/response-admin-department.dto";
+
+describe("AdminDepartment e2e", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let module: TestingModule;
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [AdminDepartmentModule],
+    }).compile();
+    app = module.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      })
+    );
+    const configService = module.get<ConfigService>(ConfigService);
+    const apiPrefix = configService.get<string>("API_PREFIX") || "api";
+    const PORT = configService.get<number>("APP_PORT") || 4010;
+    app.setGlobalPrefix(apiPrefix);
+
+    await app.init();
+    await app.listen(PORT);
+
+    prisma = app.get(PrismaService);
+    pactum.request.setBaseUrl(`http://localhost:${PORT}/${apiPrefix}`);
+  });
+
+  afterAll(async () => {
+    app.close();
+  });
+
+  describe("AdminDepartmentController createOrUpdateAdminDepartment()", () => {
+    it("should return created or updated admin department", async function () {
+      const response = await pactum
+        .spec()
+        .post("/admin-department/{departmentId}")
+        .withPathParams({ departmentId: 1 })
+        .expectStatus(201);
+      const createdAdminDepartment = JSON.parse(response);
+      expect(createdAdminDepartment).toMatchObject(ResponseAdminDepartmentDto);
+    });
+  });
+
+  describe("AdminDepartmentController getAllAdminDepartment()", () => {
+    it("should return all admin departments", async function () {
+      const response = await pactum.spec().get("/admin-departments");
+      const allAdminDepartments = JSON.parse(response);
+      expect(allAdminDepartments[0]).toMatchObject(ResponseAdminDepartmentDto);
+    });
+  });
+});
