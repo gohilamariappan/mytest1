b62948ea11f9e20c5d027a5cfbbf708fba00a142
Ajay Maurya <ajaymaurya6798@gmail.com>
2023-11-01 14:34:46 +0530

    Updated seed data

--- prisma/seeds/data/mock-rolesToCompetencies.data.ts
+++ prisma/seeds/data/mock-rolesToCompetencies.data.ts
@@ -1,18 +1,302 @@
 export const rolesToCompetencies = [
+  // For CEO - Corporate Strategy
   {
-    roleId: 1, // Linking to the first role (Network Configuration and Maintenance)
-    competencyId: 1, // Linking to the first competency (Strategic Planning)
+    roleId: 1,
+    competencyId: 1, // Strategic Planning
   },
   {
-    roleId: 2, // Linking to the second role (Security Management)
-    competencyId: 2, // Linking to the second competency (Data Analysis)
+    roleId: 1,
+    competencyId: 3, // Innovation Management
+  },
+  // For CEO - Financial Management
+  {
+    roleId: 2,
+    competencyId: 10, // Strategic Leadership
   },
   {
-    roleId: 3, // Linking to the third role (User Support)
-    competencyId: 3, // Linking to the third competency (Innovation Management)
+    roleId: 2,
+    competencyId: 3, // Innovation Management
+  },
+  // For CEO - Talent Management
+  {
+    roleId: 3,
+    competencyId: 6, // Team Collaboration
   },
   {
-    roleId: 4, // Linking to the fourth role (Performance Optimization)
-    competencyId: 4, // Linking to the fourth competency (Change Leadership)
+    roleId: 3,
+    competencyId: 10, // Strategic Leadership
+  },
+
+  // For CTO - Technology Visionary
+  {
+    roleId: 4,
+    competencyId: 1, // Strategic Planning
+  },
+  {
+    roleId: 4,
+    competencyId: 3, // Innovation Management
+  },
+  {
+    roleId: 4,
+    competencyId: 7, // Technical Expertise
+  },
+  {
+    roleId: 4,
+    competencyId: 6, // Team Collaboration
+  },
+  // For CTO - Innovation Leader
+  {
+    roleId: 5,
+    competencyId: 1, // Strategic Planning
+  },
+  {
+    roleId: 5,
+    competencyId: 3, // Innovation Management
+  },
+  {
+    roleId: 5,
+    competencyId: 7, // Technical Expertise
+  },
+  {
+    roleId: 5,
+    competencyId: 6, // Team Collaboration
+  },
+  // For CTO - Technical Advisor
+  {
+    roleId: 6,
+    competencyId: 1, // Strategic Planning
+  },
+  {
+    roleId: 6,
+    competencyId: 7, // Technical Expertise
+  },
+  {
+    roleId: 6,
+    competencyId: 9, // Problem Solving
+  },
+  // For CTO - Team Builder
+  {
+    roleId: 7,
+    competencyId: 6, // Team Collaboration
+  },
+  {
+    roleId: 7,
+    competencyId: 10, // Strategic Leadership
+  },
+  // For CIO - Cybersecurity Oversight
+  {
+    roleId: 8,
+    competencyId: 4, // Change Leadership
+  },
+  {
+    roleId: 8,
+    competencyId: 2, // Data Analysis
+  },
+  // For CIO - IT Governance
+  {
+    roleId: 9,
+    competencyId: 1, // Strategic Planning
+  },
+  {
+    roleId: 9,
+    competencyId: 7, // Technical Expertise
+  },
+  // For CIO - Resource Management
+  {
+    roleId: 10,
+    competencyId: 10, // Strategic Leadership
+  },
+  {
+    roleId: 10,
+    competencyId: 7, // Technical Expertise
+  },
+  // For VP of Engineering - Team Leadership
+  {
+    roleId: 11,
+    competencyId: 6, // Team Collaboration
+  },
+  {
+    roleId: 11,
+    competencyId: 10, // Strategic Leadership
+  },
+  // For VP of Engineering - Project Management
+  {
+    roleId: 12,
+    competencyId: 10, // Strategic Leadership
+  },
+  {
+    roleId: 12,
+    competencyId: 6, // Team Collaboration
+  },
+  // For VP of Engineering - Resource Allocation
+  {
+    roleId: 13,
+    competencyId: 10, // Strategic Leadership
+  },
+  {
+    roleId: 13,
+    competencyId: 7, // Technical Expertise
+  },
+  // For VP of Engineering - Innovation
+  {
+    roleId: 14,
+    competencyId: 3, // Innovation Management
+  },
+  {
+    roleId: 14,
+    competencyId: 6, // Team Collaboration
+  },
+  // For Software Architect - System Design
+  {
+    roleId: 15,
+    competencyId: 10, // Strategic Leadership
+  },
+  {
+    roleId: 15,
+    competencyId: 7, // Technical Expertise
+  },
+  // For Software Architect - Technology Selection
+  {
+    roleId: 16,
+    competencyId: 7, // Technical Expertise
+  },
+  {
+    roleId: 16,
+    competencyId: 6, // Team Collaboration
+  },
+  // For Software Architect - Quality Assurance
+  {
+    roleId: 17,
+    competencyId: 8, // Quality Assurance
+  },
+  {
+    roleId: 17,
+    competencyId: 9, // Problem Solving
+  },
+  // For Software Architect - Performance Optimization
+  {
+    roleId: 18,
+    competencyId: 4, // Change Leadership
+  },
+  {
+    roleId: 18,
+    competencyId: 7, // Technical Expertise
+  },
+  // For Software Architect - Team Guidance
+  {
+    roleId: 19,
+    competencyId: 6, // Team Collaboration
+  },
+  {
+    roleId: 19,
+    competencyId: 7, // Technical Expertise
+  },
+  // For Software Engineer - Software Development
+  {
+    roleId: 20,
+    competencyId: 6, // Team Collaboration
+  },
+  {
+    roleId: 20,
+    competencyId: 9, // Problem Solving
+  },
+  // For Software Engineer - Problem Solving
+  {
+    roleId: 21,
+    competencyId: 9, // Problem Solving
+  },
+  {
+    roleId: 21,
+    competencyId: 6, // Team Collaboration
+  },
+  // For Software Engineer - Collaboration
+  {
+    roleId: 22,
+    competencyId: 6, // Team Collaboration
+  },
+  {
+    roleId: 22,
+    competencyId: 10, // Strategic Leadership
+  },
+  // For QA Engineer - Test Planning and Strategy
+  {
+    roleId: 23,
+    competencyId: 1, // Strategic Planning
+  },
+  {
+    roleId: 23,
+    competencyId: 8, // Quality Assurance
+  },
+  // For QA Engineer - Test Execution
+  {
+    roleId: 24,
+    competencyId: 8, // Quality Assurance
+  },
+  {
+    roleId: 24,
+    competencyId: 9, // Problem Solving
+  },
+  // For QA Engineer - Automation Testing
+  {
+    roleId: 25,
+    competencyId: 8, // Quality Assurance
+  },
+  {
+    roleId: 25,
+    competencyId: 9, // Problem Solving
+  },
+  // For Systems Administrator - System Maintenance
+  {
+    roleId: 26,
+    competencyId: 9, // Problem Solving
+  },
+  {
+    roleId: 26,
+    competencyId: 10, // Strategic Leadership
+  },
+  // For Systems Administrator - System Security Management
+  {
+    roleId: 27,
+    competencyId: 4, // Change Leadership
+  },
+  {
+    roleId: 27,
+    competencyId: 8, // Quality Assurance
+  },
+  // For Systems Administrator - User Support
+  {
+    roleId: 28,
+    competencyId: 9, // Problem Solving
+  },
+  {
+    roleId: 28,
+    competencyId: 6, // Team Collaboration
+  },
+  // For Network Administrator - Network Configuration and Maintenance
+  {
+    roleId: 29,
+    competencyId: 1, // Strategic Planning
+  },
+  {
+    roleId: 29,
+    competencyId: 7, // Technical Expertise
+  },
+  // For Network Administrator - Security Management
+  {
+    roleId: 30,
+    competencyId: 4, // Change Leadership
+  },
+  {
+    roleId: 30,
+    competencyId: 8, // Quality Assurance
+  },
+  // For Network Administrator - Network Performance Optimization
+  {
+    roleId: 31,
+    competencyId: 4, // Change Leadership
+  },
+  {
+    roleId: 31,
+    competencyId: 7, // Technical Expertise
   },
 ];
--- prisma/seeds/data/mock-users.data.ts
+++ prisma/seeds/data/mock-users.data.ts
@@ -2,143 +2,203 @@ import { UserRolesEnum } from "@prisma/client";
 
 export const users = [
   {
+    id: "d71f8744-52b0-4ec3-9e7e-1f1266059821", // Unique UUID for Amit Sharma
     email: "amit.sharma@example.com",
     role: UserRolesEnum.ADMIN,
     userName: "Amit Sharma",
-    password: "password1",
-    designation: "Manager",
+    password: "P@ssw0rd1",
+    designation: "CEO",
+    departmentId: 1,
+    levelNumber: 4, // Assuming Amit Sharma has a competency level of "Strategic Leadership"
   },
   {
+    id: "4d45a9e9-4a4d-4c92-aaea-7b5abbd6ff98", // Unique UUID for Neha Patil
     email: "neha.patil@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Neha Patil",
-    password: "password2",
-    designation: "Employee",
+    password: "P@ssw0rd2",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 2, // competency level for Neha Patil
   },
   {
+    id: "abaa7220-5d2e-4e05-842a-95b2c4ce1876", // Unique UUID for Rajesh Kumar
     email: "rajesh.kumar@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Rajesh Kumar",
-    password: "password3",
-    designation: "Employee",
+    password: "P@ssw0rd3",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 3, // competency level for Rajesh Kumar
   },
   {
+    id: "0f5d0b13-8d72-46c9-a7c4-c1f7e5aa1f17", // Unique UUID for Priya Jain
     email: "priya.jain@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Priya Jain",
-    password: "password4",
-    designation: "Employee",
+    password: "P@ssw0rd4",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 2, // competency level for Priya Jain
   },
   {
+    id: "bbf1f7cf-4216-458e-8d98-0d9204ae57ef", // Unique UUID for Arjun Singh
     email: "arjun.singh@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Arjun Singh",
-    password: "password5",
-    designation: "Employee",
+    password: "P@ssw0rd5",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 3, // competency level for Arjun Singh
   },
   {
+    id: "a3bf8f0e-22a9-4a5e-8c76-e80b67a1a6e9", // Unique UUID for Deepa Mishra
     email: "deepa.mishra@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Deepa Mishra",
-    password: "password6",
-    designation: "Employee",
+    password: "P@ssw0rd6",
+    designation: "Software Architect",
+    departmentId: 1,
+    levelNumber: 4, // competency level for Deepa Mishra
   },
   {
+    id: "ac6b9e42-efea-401a-bd67-9cf9110ac3c9", // Unique UUID for Rahul Verma
     email: "rahul.verma@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Rahul Verma",
-    password: "password7",
-    designation: "Employee",
+    password: "P@ssw0rd7",
+    designation: "Software Architect",
+    departmentId: 1,
+    levelNumber: 3, // competency level for Rahul Verma
   },
   {
+    id: "0f82ac61-9801-488f-babb-3c2d23a413d0", // Unique UUID for Anita Sen
     email: "anita.sen@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Anita Sen",
-    password: "password8",
-    designation: "Employee",
+    password: "P@ssw0rd8",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 2, // competency level for Anita Sen
   },
   {
+    id: "69d7871b-3b09-4b5b-9ea0-0ca27c2ac9e3", // Unique UUID for Mohit Gupta
     email: "mohit.gupta@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Mohit Gupta",
-    password: "password9",
-    designation: "Employee",
+    password: "P@ssw0rd9",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 3, // competency level for Mohit Gupta
   },
   {
+    id: "7b8005e4-e3d5-4a0d-81d6-d2965a9d5c2c", // Unique UUID for Pooja Rawat
     email: "pooja.rawat@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Pooja Rawat",
-    password: "password10",
-    designation: "Employee",
+    password: "P@ssw0rd10",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 3, // competency level for Pooja Rawat
   },
   {
+    id: "1536c7ad-234e-44d3-b6d4-8ca655b46e2a", // Unique UUID for Anurag Sharma
     email: "anurag.sharma@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Anurag Sharma",
-    password: "password11",
-    designation: "Employee",
+    password: "P@ssw0rd11",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 2, // competency level for Anurag Sharma
   },
   {
+    id: "9a1f8f5a-0d9e-40e2-b7ea-df493a303db7", // Unique UUID for Meera Rani
     email: "meera.rani@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Meera Rani",
-    password: "password12",
-    designation: "Employee",
+    password: "P@ssw0rd12",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 2, // competency level for Meera Rani
   },
   {
+    id: "fbd6d6b9-4c4f-48c1-9147-3006a369e190", // Unique UUID for Vikas Patel
     email: "vikas.patel@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Vikas Patel",
-    password: "password13",
-    designation: "Employee",
+    password: "P@ssw0rd13",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 3, // competency level for Vikas Patel
   },
   {
+    id: "4f67ae5a-c2a2-4652-81cc-4d1471e1a158", // Unique UUID for Divya Malik
     email: "divya.malik@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Divya Malik",
-    password: "password14",
-    designation: "Employee",
+    password: "P@ssw0rd14",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 2, // competency level for Divya Malik
   },
   {
+    id: "a6a862ac-f5f7-4d6b-a297-c1497e5652e5", // Unique UUID for Rajiv Khanna
     email: "rajiv.khanna@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Rajiv Khanna",
-    password: "password15",
-    designation: "Employee",
+    password: "P@ssw0rd15",
+    designation: "Software Engineer",
+    departmentId: 1,
+    levelNumber: 4, // competency level for Rajiv Khanna
   },
   {
+    id: "72e9272e-3a4e-45f6-877c-6567f3f9785a", // Unique UUID for Swati Shah
     email: "swati.shah@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Swati Shah",
-    password: "password16",
-    designation: "Employee",
+    password: "P@ssw0rd16",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 3, // competency level for Swati Shah
   },
   {
+    id: "5d0b5fb7-d350-4315-988e-df878e96287d", // Unique UUID for Manish Agrawal
     email: "manish.agrawal@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Manish Agrawal",
-    password: "password17",
-    designation: "Employee",
+    password: "P@ssw0rd17",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 4, // competency level for Manish Agrawal
   },
   {
+    id: "a1f3e09a-369f-4e7b-997b-891274f1020e", // Unique UUID for Pallavi Reddy
     email: "pallavi.reddy@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Pallavi Reddy",
-    password: "password18",
-    designation: "Employee",
+    password: "P@ssw0rd18",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 2, // competency level for Pallavi Reddy
   },
   {
+    id: "d35d1e80-d2df-4b6a-9a67-8ef5f2877a57", // Unique UUID for Deepak Mishra
     email: "deepak.mishra@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Deepak Mishra",
-    password: "password19",
-    designation: "Employee",
+    password: "P@ssw0rd19",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 4, // competency level for Deepak Mishra
   },
   {
+    id: "7646a865-513f-4529-8ecf-170220e2d6b8", // Unique UUID for Komal Choudhary
     email: "komal.choudhary@example.com",
     role: UserRolesEnum.CONSUMER,
     userName: "Komal Choudhary",
-    password: "password20",
-    designation: "Employee",
+    password: "P@ssw0rd20",
+    designation: "QA Engineer",
+    departmentId: 3,
+    levelNumber: 2, // competency level for Komal Choudhary
   },
 ];
