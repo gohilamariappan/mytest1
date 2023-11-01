e35911c706c7dcf593c1a549533869f2c42ae8bb
VamshiBatta07 <batta.v@esmagico.in>
2023-11-01 11:42:30 +0530

    Seeds for Mock: User, Designation, Department and Roles.

---
+++ prisma/seeds/data/mock-designation.data.ts
@@ -0,0 +1,47 @@
+export const designations = [
+  {
+    id: 1,
+    name: "CEO",
+    description: "Chief Executive Officer",
+  },
+  {
+    id: 2,
+    name: "CTO",
+    description: "Chief Technology Officer",
+  },
+  {
+    id: 3,
+    name: "CIO",
+    description: "Chief Information Officer",
+  },
+  {
+    id: 4,
+    name: "VP of Engineering",
+    description: "Vice President of Engineering",
+  },
+  {
+    id: 5,
+    name: "Software Architect",
+    description: "Senior Software Architect",
+  },
+  {
+    id: 6,
+    name: "Software Engineer",
+    description: "Software Development Engineer",
+  },
+  {
+    id: 7,
+    name: "QA Engineer",
+    description: "Quality Assurance Engineer",
+  },
+  {
+    id: 8,
+    name: "Systems Administrator",
+    description: "IT Systems Administrator",
+  },
+  {
+    id: 9,
+    name: "Network Administrator",
+    description: "Network Systems Administrator",
+  }
+];
---
+++ prisma/seeds/data/mock-roles.data.ts
@@ -0,0 +1,165 @@
+export const roles = [
+  //For CEO
+  {
+    name: "Corporate Strategy",
+    description:
+      "Developing and overseeing the organization's strategic plan to achieve its objectives.",
+  },
+  {
+    name: "Financial Management",
+    description:
+      "Managing the company's finances, including budgeting, financial planning, and financial risk management.",
+  },
+  {
+    name: "Talent Management",
+    description:
+      "Attracting, retaining, and developing top talent within the organization.",
+  },
+
+  //For CTO
+  {
+    name: "Technology Visionary",
+    description:
+      "The CTO defines the organization's long-term technology strategy, ensuring alignment with business goals and emerging industry trends.",
+  },
+  {
+    name: "Innovation Leader",
+    description:
+      "Responsible for fostering innovation, the CTO drives research and development efforts to create cutting-edge solutions and stay competitive.",
+  },
+  {
+    name: "Technical Advisor",
+    description:
+      "Provides technical expertise to guide decision-making, assess risks, and choose the right technologies for projects.",
+  },
+  {
+    name: "Team Builder",
+    description:
+      "Recruits and manages top technical talent, fostering a culture of excellence and professional growth within the organization.",
+  },
+
+  //CIO
+  {
+    name: "Cybersecurity Oversight",
+    description:
+      "The CIO plays a critical role in safeguarding the organization's digital assets, implementing security measures, and mitigating cybersecurity threats.",
+  },
+  {
+    name: "IT Governance",
+    description:
+      "The CIO establishes IT governance frameworks, policies, and procedures to ensure efficient and compliant IT operations within the organization.",
+  },
+  {
+    name: "Resource Management",
+    description:
+      "CIOs manage IT budgets, resources, and teams, optimizing technology investments and ensuring IT projects are delivered on time and within budget.",
+  },
+
+  //VP of Engineering
+  {
+    name: "Team Leadership",
+    description: "Managing and mentoring engineering teams.",
+  },
+  {
+    name: "Project Management",
+    description: "Overseeing project execution and delivery.",
+  },
+  {
+    name: "Resource Allocation",
+    description: "Efficiently allocating resources and budgets.",
+  },
+  {
+    name: "Innovation",
+    description:
+      "Promoting innovation and staying current with technology trends.",
+  },
+
+  //Software Architect
+  {
+    name: "System Design",
+    description: "Creating high-level system architecture and design.",
+  },
+  {
+    name: "Technology Selection",
+    description: "Choosing the right tech stack for projects.",
+  },
+  {
+    name: "Quality Assurance",
+    description: "Ensuring software meets quality standards.",
+  },
+  {
+    name: "Performance Optimization",
+    description: "Improving system performance.",
+  },
+  {
+    name: "Team Guidance",
+    description: "Providing technical leadership and mentoring.",
+  },
+
+  //Software Engineer
+  {
+    name: "Software Development",
+    description: "Writing, testing, and maintaining software applications.",
+  },
+  {
+    name: "Problem Solving",
+    description: "Identifying and resolving technical challenges.",
+  },
+  {
+    name: "Collaboration",
+    description: "Working with cross-functional teams to deliver projects.",
+  },
+
+  //QA Engineer
+  {
+    name: "Test Planning and Strategy",
+    description:
+      "Creating test plans and strategies to ensure comprehensive testing of software.",
+  },
+  {
+    name: "Test Execution",
+    description:
+      "Performing testing, identifying defects, and ensuring software quality.",
+  },
+  {
+    name: "Automation Testing",
+    description:
+      "Developing and maintaining automated test scripts to improve efficiency and coverage.",
+  },
+
+  //Systems Administrator
+  {
+    name: "System Maintenance",
+    description:
+      "Managing and maintaining IT infrastructure to ensure it runs smoothly.",
+  },
+  {
+    name: "System Security Management",
+    description:
+      "Protecting systems and data from threats and vulnerabilities.",
+  },
+  {
+    name: "User Support",
+    description:
+      "Providing technical assistance and troubleshooting for employees.",
+  },
+
+  //Network Administrator
+  {
+    name: "Network Configuration and Maintenance",
+    description: "Setting up and maintaining network infrastructure",
+  },
+  {
+    name: "Security Management",
+    description:
+      "Protecting the network from cyber threats and vulnerabilities",
+  },
+//   {
+//     name: "User Support",
+//     description: "Assisting users with network-related issues",
+//   },
+  {
+    name: "Network Performance Optimization",
+    description: "Ensuring network efficiency and reliability",
+  },
+];
