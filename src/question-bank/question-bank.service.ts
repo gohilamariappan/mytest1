4340c261a343ae62cc7ef0b3a2ec4d4d32487b3e
Prashant Kumar <144701828+prashantesmagico@users.noreply.github.com>
2023-11-14 15:13:51 +0530

    API for question bank template

--- src/question-bank/question-bank.service.ts
+++ src/question-bank/question-bank.service.ts
@@ -362,4 +362,9 @@ export class QuestionBankService {
       }
     });
   }
+
+  public async getQuestionBankTemplate() {
+    const header = ["competency", "competencyLevelNumber", "question"];
+    return header;
+  }
 }
